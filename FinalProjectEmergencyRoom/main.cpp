1285831aabb270875691aedb5fbab167c37a028f
Chris Roberts <legotfol@gmail.com>
2018-12-07 09:51:27 -0800

    MAIN IS A THING

--- FinalProjectEmergencyRoom/main.cpp
+++ FinalProjectEmergencyRoom/main.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+
 #include "Doctor.h"
 #include "Nurse.h"
 #include "Record.h"
@@ -7,6 +9,15 @@ int main() {
 	//fileio here
 	//for loop 7x24x60 random chance to add someone to queue
 	
+	double rate;
+	int doc, nurse;
+
+	std::cout << "Enter average hourly patient arrival rate, the number of Doctors working, and the number of Nurses working\n";
+
+	std::cin >> rate >> doc >> nurse;
+
+	rate = 60 / rate;
+
 
 	
 
