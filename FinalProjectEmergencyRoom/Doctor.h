d8d9abb9e2f941adcf645c68e47c73defec9c9da
Chris Roberts <legotfol@gmail.com>
2018-12-07 17:14:45 -0800

    Comments for Caregiver done and Doctor started

--- FinalProjectEmergencyRoom/Doctor.h
+++ FinalProjectEmergencyRoom/Doctor.h
@@ -1,8 +1,8 @@
 //////////////////////////////////
-//Title:
+//Title: Doctor.h
 //Authors: Abby Jahns, Aiden McIlraith, Chris Roberts
 //Last Modified: 12/7/2018
-//Purpose:
+//Purpose: Implement Doctor Class
 //////////////////////////////////
 
 #ifndef DOCTOR_H
--- FinalProjectEmergencyRoom/caregiver.h
+++ FinalProjectEmergencyRoom/caregiver.h
@@ -1,8 +1,8 @@
 //////////////////////////////////
-//Title:
+//Title: Caregiver.h
 //Authors: Abby Jahns, Aiden McIlraith, Chris Roberts
 //Last Modified: 12/7/2018
-//Purpose:
+//Purpose: Implement Caregiver Class
 //////////////////////////////////
 
 
@@ -14,13 +14,27 @@
 class Caregiver
 {
 public:
+
+	//constructor
 	Caregiver() {}
+
+	//destructor
 	virtual ~Caregiver() {}
+	
+	//pulls a patient off of the queue and adds it to either
+	//a doctor or a nurse depending on who calls the function
 	virtual void addPatient(PatientQueue&, Record&) = 0;
+
+	//returns true if the doctor or nurse that calls the
+	//function is currently treating a patient  
 	virtual bool hasPatient() { return timeRemaining > 0; }
+
+	//returns current patient
 	virtual Patient& getPatient() { return currentPatient; }
 
+	//decreases the time reamining to treat the current patient
 	void decTime() { timeRemaining--; }
+
 protected:
 	Patient currentPatient;
 	int timeRemaining;
