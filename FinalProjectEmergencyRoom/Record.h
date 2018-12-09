369fdd545bb8a336952b93fe47127c57d541a34a
Chris Roberts <legotfol@gmail.com>
2018-12-08 22:04:35 -0800

    Oh look what I found... More comments! Yay!

--- FinalProjectEmergencyRoom/Record.h
+++ FinalProjectEmergencyRoom/Record.h
@@ -22,17 +22,22 @@ public:
 	//deconstructor
 	~Record() {}
 
-	//
+	//adds the name and severity of a patient to the record
 	void addName(Patient input) { addName(input.getName(), input.getSeverity()); }
 
+	//adds a specified name and severity to the record
 	void addName(std::string, int severity);
 
+	//returns the number of visits for a specified name
 	int getNumVisits(std::string input);
 
+	//returns how many people are in the record
 	int numPeople() { return data.size(); }
 
+	//displays all of the data stored in the record
 	void display();
 
+	//returns the severeity of a specific name on a specific visit
 	int getSeverityOfVisit(std::string name, int visit);
 private:
 	std::map<std::string, std::vector<int>> data;
